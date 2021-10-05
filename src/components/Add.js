4cbeddb939d2f7eaf0938b3189604ca37b00c076
alberto <medina.v@pm.me>
2021-10-05 18:38:53 +0100

    abasic from to add contacts

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function Add() {
   return (
-    <div>
-      HELLLOO 
-    </div>
-  )
+			<div className='ui main'>
+				<h3>Add Contact</h3>
+				<form className='ui form'>
+					<div className='field'>
+						<label>Name</label>
+						<input type='text' name='name' placeholder='Name' />
+					</div>
+					<div className='field'>
+						<label>Email</label>
+						<input type='text' name='Email' placeholder='Email' />
+					</div>
+          <button className="ui button green">Add</button>
+				</form>
+			</div>
+		);
 }
