399659b6b165a8edbd18b0e29f266964494ffa13
alberto <medina.v@pm.me>
2021-10-06 20:32:35 +0100

    setup state for the name and email

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import Header from './components/Header';
 import List from './components/List';
 
 function App() {
-
-  const contacts = [
-			{
-				id: 1,
-				name: 'Albert',
-				email: 'albert@pm.me',
-			},
-			{
-				id: 2,
-				name: 'Steve',
-				email: 'steve@pm.me',
-			},
-			{
-				id: 3,
-				name: 'Tom',
-				email: 'tom@pm.me',
-			},
-		];
+  const [contacts, setContacts] = useState([])
 
 
   return (
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function Add() {
-  return (
-			<div className='ui main'>
-				<h3>Add Contact</h3>
-				<form className='ui form'>
-					<div className='field'>
-						<label>Name</label>
-						<input type='text' name='name' placeholder='Name' />
-					</div>
-					<div className='field'>
-						<label>Email</label>
-						<input type='text' name='Email' placeholder='Email' />
-					</div>
-          <button className="ui button green">Add</button>
-				</form>
-			</div>
-		);
+	// state to manage users' name and email
+	const [info, setInfo] = useState({ name: '', email: '' });
+
+	// tracks user's name and syncs it with state
+	const handleChangeName = (event) => {
+    console.log(event.target.value);
+		setInfo({
+			...info,
+			[event.target.name]: event.target.value,
+		});
+	};
+
+
+
+  // prevents page from page from reshesing after every submit
+	const onSubmitForm = (event) => {
+		event.preventDefault();
+    console.log(info)
+	};
+
+	return (
+		<div className='ui main'>
+			<h3>Add Contact</h3>
+			<form className='ui form' onSubmit={onSubmitForm}>
+				<div className='field'>
+					<label>Name</label>
+					<input
+						type='text'
+						name='name'
+						placeholder='Name'
+            required
+						value={info.name}
+						onChange={handleChangeName}
+					/>
+				</div>
+				<div className='field'>
+					<label>Email</label>
+					<input
+						type='text'
+						name='email'
+            required
+						placeholder='Email'
+						value={info.email}
+						onChange={handleChangeName}
+					/>
+				</div>
+				<button className='ui button green'>Add</button>
+			</form>
+		</div>
+	);
 }
